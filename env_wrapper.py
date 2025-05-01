2720aae9d967fca96cd9f6179501280c0c8e0ba9
C-W-Z <chenweizhang3021@gmail.com>
2025-05-01 14:31:52 +0800

    update wrappers

--- env_wrapper.py
+++ env_wrapper.py
@@ -1,10 +1,42 @@
 import cv2
 import gym
-from gym.wrappers import TimeLimit
+from gym.wrappers import FrameStack, TimeLimit
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
 import numpy as np
+
+class NoopResetEnv(gym.Wrapper):
+    """
+    From https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
+    """
+    def __init__(self, env, noop_max=30):
+        """Sample initial states by taking random number of no-ops on reset.
+        No-op is assumed to be action 0.
+        """
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1) #pylint: disable=E1101
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, action):
+        return self.env.step(action)
 
 class RandomStartEnv(gym.Wrapper):
     def __init__(self, env, random_steps=4):
@@ -30,7 +62,8 @@ class SkipAndMax(gym.Wrapper):
         done = None
         for i in range(self._skip):
             obs, reward, done, info = self.env.step(action)
-            self._obs_buffer[i & 1] = np.asarray(obs)
+            if i >= self._skip - 2:
+                self._obs_buffer[i & 1] = np.asarray(obs)
             total_reward += reward
             if done:
                 break
@@ -38,10 +71,7 @@ class SkipAndMax(gym.Wrapper):
         return max_frame, total_reward, done, info
 
     def reset(self):
-        obs = self.env.reset()
-        obs = np.asarray(obs)
-        self._obs_buffer[0] = self._obs_buffer[1] = obs
-        return obs
+        return self.env.reset()
 
 class LifeEpisode(gym.Wrapper):
     def __init__(self, env):
@@ -69,7 +99,7 @@ class LifeEpisode(gym.Wrapper):
 class FrameProcessing(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
-        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(1, 84, 84), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(84, 84), dtype=np.float32)
 
     def observation(self, obs):
         return FrameProcessing.process(obs)
@@ -83,42 +113,26 @@ class FrameProcessing(gym.ObservationWrapper):
         # 縮放到 (110, 84)，然後裁剪
         frame = cv2.resize(frame, (84, 110), interpolation=cv2.INTER_AREA)
         frame = frame[18:102, :]  # (84, 84)
-        # 轉為 (1, 84, 84)，規範化到 [0.0, 1.0]
-        return frame.astype(np.float32)[np.newaxis, :, :] / 255.0
-        # assert frame.shape == (1, 84, 84)
-
-class FrameStack(gym.Wrapper):
-    def __init__(self, env, n_steps=4):
-        super().__init__(env)
-        self.n_steps = n_steps
-        shp = env.observation_space.shape
-        self.observation_space = gym.spaces.Box(0, 1, shape=(shp[0] * n_steps, shp[1], shp[2]), dtype=np.float32)
-        self.frames = np.zeros(self.observation_space.shape, dtype=np.float32)
-
-    def reset(self):
-        obs = self.env.reset()
-        obs = np.asarray(obs)
-        for i in range(self.n_steps):
-            self.frames[i] = obs
-        return self.frames
-
-    def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        obs = np.asarray(obs)
-        self.frames[:-1] = self.frames[1:]
-        self.frames[-1] = obs
-        return self.frames, reward, done, info
+        # 規範化到 [0.0, 1.0]
+        return frame.astype(np.float32) / 255.0
 
 def make_env(skip_frames=4, stack_frames=4, max_episode_steps=None, life_episode=True, random_start=False, level='1-1'):
     env = gym_super_mario_bros.make(f'SuperMarioBros-{level}-v0' if level else 'SuperMarioBros-v0')
     env = JoypadSpace(env, COMPLEX_MOVEMENT)
+    env = NoopResetEnv(env)
     # if random_start:
     #     env = RandomStartEnv(env, random_steps=4)
     env = SkipAndMax(env, skip=skip_frames)
     if life_episode:
         env = LifeEpisode(env)
-    env = FrameProcessing(env)
-    env = FrameStack(env, n_steps=stack_frames)
+    env = FrameProcessing(env) # (84, 84)
+    env = FrameStack(env, num_stack=stack_frames) # (4, 84, 84)
     # if max_episode_steps:
     #     env = TimeLimit(env, max_episode_steps=max_episode_steps)
     return env
+
+if __name__ == "__main__":
+    env = make_env(life_episode=False, level='')
+    print(f"observation_space.shape: {env.observation_space.shape}")
+    obs = env.reset()
+    assert obs.shape == env.observation_space.shape
